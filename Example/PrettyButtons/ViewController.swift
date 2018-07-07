d971269404c262837f3cf5c17c8bb11dd0a32f71
chrisfromtemporaryid@gmail.com <chrisfromtemporaryid@gmail.com>
2018-07-06 17:38:37 -0700

    Initial commit

---
+++ Example/PrettyButtons/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  PrettyButtons
+//
+//  Created by chrisfromtemporaryid@gmail.com on 07/06/2018.
+//  Copyright (c) 2018 chrisfromtemporaryid@gmail.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
