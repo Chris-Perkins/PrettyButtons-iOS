b2b2b9672fca7398329394fa5ae6784591e2fca9
chrisfromtemporaryid@gmail.com <chrisfromtemporaryid@gmail.com>
2018-07-06 18:18:03 -0700

    Initial Release

---
+++ Example/Pods/Target Support Files/Pods-PrettyButtons_Example/Pods-PrettyButtons_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_PrettyButtons_Example : NSObject
+@end
+@implementation PodsDummy_Pods_PrettyButtons_Example
+@end
--- Example/PrettyButtons/ViewController.swift
+++ Example/PrettyButtons/ViewController.swift
@@ -7,18 +7,46 @@
 //
 
 import UIKit
+import PrettyButtons
 
 class ViewController: UIViewController {
+    /// A nice blue color.
+    private static let niceBlue = UIColor(red: 0.29, green: 0.46, blue: 0.91, alpha: 1)
+    
+    /// A nice orange color.
+    private static let niceOrange = UIColor(red: 0.90, green: 0.70, blue: 0.16, alpha: 1)
 
+    /// Creates a view in the center and buttons for touch-feedback.
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // Create a view in the center
+        let centerView = UIView()
+        view.addSubview(centerView)
+        centerView.copy(.centerX, .centerY, of: view)
+        centerView.copy(.width, of: view).withMultiplier(0.75)
+        centerView.copy(.height, of: view).withMultiplier(0.75)
+        
+        // Then create the buttons that will be in the center view
+        let noStyleButton = PrettyButton()
+        noStyleButton.setTitle("No Style", for: .normal)
+        let slideStyleButton = PrettyButton(style: .slide)
+        slideStyleButton.setTitle("Slide ->", for: .normal)
+        let bloomStyleButton = PrettyButton(style: .bloom)
+        bloomStyleButton.setTitle("Bloom <^>", for: .normal)
+        let fadeStyleButton = PrettyButton(style: .fade)
+        fadeStyleButton.setTitle("Fade *", for: .normal)
+        
+        let buttons = [noStyleButton, slideStyleButton, bloomStyleButton, fadeStyleButton]
+        for button in buttons {
+            centerView.addSubview(button)
+            
+            button.copy(.left, .right, of: centerView)
+            button.backgroundColor = ViewController.niceBlue
+            button.overlayColor = ViewController.niceOrange
+        }
+        
+        centerView.fill(.topToBottom, withViews: buttons, withSpacing: 15)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 }
 
